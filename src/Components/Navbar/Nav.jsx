594b01a0e7dc0c86e9b9f071ef245a88e5f36b04
Shreeram Yadav <yadavshreeram147@gmail.com>
2024-09-17 09:17:42 +0530

    added some basic html

--- src/Components/Navbar/Nav.jsx
+++ src/Components/Navbar/Nav.jsx
@@ -12,18 +12,19 @@ const Nav = () => {
 
   return (
     <div className='flex flex-wrap justify-between items-center w-full fixed top-0 left-0 p-4 bg-slate-600 rounded-lg ml-1 mr-1 z-50'>
-      
-      <div className='w-48 h-14'>
-        <Logo />
+      <div className='flex justify-between items-center w-full'>
+        <div className='w-48 h-14'>
+          <Logo />
+        </div>
+
+        {/* Hamburger Menu for Mobile */}
+        <button className='md:hidden text-white' onClick={toggleMenu}>
+          {isMenuOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
+        </button>
       </div>
 
-      {/* Hamburger Menu for Mobile */}
-      <button className='md:hidden text-white' onClick={toggleMenu}>
-        {isMenuOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
-      </button>
-
       {/* Navigation Links */}
-      <nav className={`${isMenuOpen ? 'flex' : 'hidden'} md:flex flex-col md:flex-row justify-center space-y-4 md:space-y-0 md:space-x-14 text-lg text-white w-full md:w-auto mt-4 md:mt-0`}>
+      <nav className={`${isMenuOpen ? 'flex' : 'hidden'} md:flex flex-col md:flex-row justify-center items-center space-y-4 md:space-y-0 md:space-x-14 text-lg text-white w-full md:w-auto mt-4 md:mt-0`}>
         <NavLink
           className={(navData) => navData.isActive ? 'scale-110 underline' : ''}
           to="/"
@@ -45,11 +46,11 @@ const Nav = () => {
       </nav>
 
       {/* Buttons */}
-      <div className={`${isMenuOpen ? 'flex' : 'hidden'} md:flex space-x-3 mt-4 md:mt-0 w-full md:w-auto justify-center md:justify-end`}>
-        <button className='py-1 px-5 text-white rounded-md text-lg bg-green-500 hover:bg-green-600'>
+      <div className={`${isMenuOpen ? 'flex' : 'hidden'} md:flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-3 mt-4 md:mt-0 w-full md:w-auto items-center md:justify-end`}>
+        <button className='w-full md:w-auto py-1 px-5 text-white rounded-md text-lg bg-green-500 hover:bg-green-600'>
           Log In
         </button>
-        <button className='py-1 px-5 text-white rounded-md text-lg bg-green-500 hover:bg-green-600'>
+        <button className='w-full md:w-auto py-1 px-5 text-white rounded-md text-lg bg-green-500 hover:bg-green-600'>
           Sign Up
         </button>
       </div>
